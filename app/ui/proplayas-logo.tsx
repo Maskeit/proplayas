85f57c910f39f357dc06154ffe551e92ef358f08
Miguel <miguelagustin182@gmail.com>
2024-04-30 22:08:14 -0600

    feat: he creado el comienzo del dashboard

---
+++ app/ui/proplayas-logo.tsx
@@ -0,0 +1,12 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { opens } from '@/app/ui/fonts';
+import logo from '@/public/proplayas_logo.svg'
+import Image from 'next/image';
+export default function ProplayasLogo() {
+  return (
+    <div className={`${opens.className} flex flex-row items-center justify-around leading-none text-white`}>
+      <Image src={logo} width={60} alt='Logo_proplayas'/>
+      <p className="text-[30px] ml-2">Proplayas</p>
+    </div>
+  );
+}
