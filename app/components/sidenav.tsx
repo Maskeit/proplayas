f2e3c4d1564ce792059c6fe856e5c6c9da5ca77c
Miguel <miguelagustin182@gmail.com>
2024-05-05 18:48:31 -0600

    fix: diseño responsive del banner y sidenav

--- app/components/sidenav.tsx
+++ app/components/sidenav.tsx
@@ -1,35 +1,36 @@
 import Link from 'next/link';
-import '../components/sidenav.css'
+import '../components/sidenav.css';
 import NavLinks from '@/app/components/nav-links';
 import Proplayas, { ProplayasSidebar } from '@/app/components/proplayas-logo';
-import { PowerIcon, ArrowLeftIcon, UserCircleIcon } from '@heroicons/react/24/outline';
-import { CloseMenu } from './buttons'; 
+import {
+  PowerIcon,
+  ArrowLeftIcon,
+  UserCircleIcon,
+} from '@heroicons/react/24/outline';
+import { CloseMenu } from './buttons';
 
-
-export default function SideNav({toggleSidebar, isActive}) {
+export default function SideNav({ toggleSidebar, isActive }) {
   const sidebarClass = `sidenav ${isActive ? 'active' : ''}`;
   return (
     <aside className={sidebarClass}>
-      <div className={`flex h-full flex-col px-3 py-4 md:px-2 `} >
-        <div className=' mb-2 h-10 items-center flex justify-between bg-white rounded-md p-4 md:h-20'>
-
-        <Link
-          href="/">
-          <div className="flex w-32 gap-9 md:w-40 ">
-            <ProplayasSidebar />
-          </div>
-        </Link>
-        <CloseMenu toggleSidebar={toggleSidebar}/>
+      <div className={`flex h-full flex-col px-3 py-4 md:px-2 `}>
+        <div className="mb-2 flex h-20 items-center justify-between gap-9 rounded-md bg-white p-4 ">
+          <Link href="/">
+            <div className="flex w-40 ">
+              <ProplayasSidebar />
+            </div>
+          </Link>
+          <CloseMenu toggleSidebar={toggleSidebar} />
         </div>
-        <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <div className="flex grow flex-col justify-between space-x-0 space-y-2">
           <NavLinks />
-          <div className="hidden h-auto w-full grow rounded-md  md:block"></div>
+          <div className=" block h-auto w-full grow  rounded-md"></div>
           <form>
             <Link href="/login">
-            <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-                <PowerIcon className="w-6"/>
+              <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+                <PowerIcon className="w-6" />
                 <p>Iniciar sesion</p>
-            </button>
+              </button>
             </Link>
           </form>
         </div>
@@ -38,20 +39,18 @@ export default function SideNav({toggleSidebar, isActive}) {
   );
 }
 
-export  const SideNavDash = ({toggleSidebar, isActive}) => {
+export const SideNavDash = ({ toggleSidebar, isActive }) => {
   const sidebarClass = `sidenav ${isActive ? 'active' : ''}`;
   return (
     <aside className={sidebarClass}>
-      <div className={`flex h-full flex-col px-3 py-4 md:px-2 `} >
-        <div className=' mb-2 h-10 items-center flex justify-between bg-white rounded-md p-4 md:h-20'>
-
-        <Link
-          href="/">
-          <div className="flex w-32 gap-9 md:w-40 ">
-            <ProplayasSidebar />
-          </div>
-        </Link>
-        <CloseMenu toggleSidebar={toggleSidebar}/>
+      <div className={`flex h-full flex-col px-3 py-4 md:px-2 `}>
+        <div className=" mb-2 flex h-10 items-center justify-between rounded-md bg-white p-4 md:h-20">
+          <Link href="/">
+            <div className="flex w-32 gap-9 md:w-40 ">
+              <ProplayasSidebar />
+            </div>
+          </Link>
+          <CloseMenu toggleSidebar={toggleSidebar} />
         </div>
 
         <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
@@ -66,5 +65,4 @@ export  const SideNavDash = ({toggleSidebar, isActive}) => {
       </div>
     </aside>
   );
-}
-
+};
