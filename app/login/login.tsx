64b5f008da5e6b0b85afe533b8869960149e0b1e
Miguel <miguelagustin182@gmail.com>
2024-10-25 16:02:26 -0600

    feat: login user

--- app/login/login.tsx
+++ app/login/login.tsx
@@ -1,42 +1,103 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation'; // Este import es clave para que funcione en el directorio app
 import styles from './login.module.css';
-import { LockClosedIcon, AtSymbolIcon } from '@heroicons/react/24/outline';
-import {ArrowRightOnRectangleIcon} from '@heroicons/react/24/outline';
+import { LockClosedIcon, AtSymbolIcon, ArrowRightOnRectangleIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-export const LoginForm = ()=>{
-  return (
-    <div className={`${styles.loginContainer}`}>
 
-        <div className={styles.loginBox}>
-            <h1 className={styles.loginTitle}>Inicia Sesión</h1>
-            <form className={styles.loginForm}>
-            <div className={styles.inputGroup}>
-                <label className={styles.label} htmlFor="email">Correo electrónico</label>
-                <input type="email" id='email' name='email' placeholder="" className={styles.inputField} required />
-                <div className={styles.icon}>
-                    <AtSymbolIcon width='24px'/>
-                </div>
+export const LoginForm = () => {
+    const [userData, setUserData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const router = useRouter();
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setUserData({ ...userData, [name]: value });
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+    
+        const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';  // fallback al entorno local
+    
+        try {
+            const response = await fetch("http://localhost:8000/auth", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify(userData)
+            });
+    
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+    
+            const data = await response.json(); // Obtener el token de la respuesta
+            console.log('Token:', data.token);
+    
+            // Guardar el token en localStorage o sessionStorage
+            localStorage.setItem('token', data.token);
+    
+            // Redirigir al usuario a la página /home
+            router.push('/home');
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Error al iniciar sesión, revisa tus credenciales.');
+        }
+    };    
+
+    return (
+        <div className={`${styles.loginContainer}`}>
+            <div className={styles.loginBox}>
+                <h1 className={styles.loginTitle}>Inicia Sesión</h1>
+                <form className={styles.loginForm} onSubmit={handleSubmit}>
+                    <div className={styles.inputGroup}>
+                        <label className={styles.label} htmlFor="email">Correo electrónico</label>
+                        <input
+                            type="email"
+                            id='email'
+                            name='email'
+                            placeholder=""
+                            className={styles.inputField}
+                            required
+                            onChange={handleChange}
+                        />
+                        <div className={styles.icon}>
+                            <AtSymbolIcon width='24px' />
+                        </div>
+                    </div>
+                    <div className={styles.inputGroup}>
+                        <label className={styles.label} htmlFor="password">Contraseña</label>
+                        <input
+                            type="password"
+                            name='password'
+                            id='password'
+                            className={styles.inputField}
+                            required
+                            onChange={handleChange}
+                        />
+                        <div className={styles.icon}>
+                            <LockClosedIcon width='24px' />
+                        </div>
+                    </div>
+                    <div className={styles.checkboxGroup}>
+                        <input type="checkbox" id="rememberMe" className={styles.checkbox} />
+                        <label htmlFor="rememberMe" className={styles.checkboxLabel}>Recordarme</label>
+                    </div>
+                    <button type="submit" className={styles.loginButton}>
+                        Iniciar sesión
+                        <ArrowRightOnRectangleIcon color='#pink' width='24px' />
+                    </button>
+                    <Link href='/login/forgot' className={styles.forgotPassword}>
+                        Olvidé mi contraseña
+                    </Link>
+                </form>
             </div>
-            <div className={styles.inputGroup}>
-                <label className={styles.label} htmlFor="password">Contraseña</label>
-                <input  type="password" name='password' id='password'  className={styles.inputField} required/>
-                <div className={styles.icon}>
-                    <LockClosedIcon width='24px'/>
-                </div>
-            </div>
-            <div className={styles.checkboxGroup}>
-                <input type="checkbox" id="rememberMe" className={styles.checkbox} />
-                <label htmlFor="rememberMe" className={styles.checkboxLabel}>Recordarme</label>
-            </div>
-            <button type="submit" className={styles.loginButton}>
-                Iniciar sesión  
-                <ArrowRightOnRectangleIcon color='#pink' width='24px' />
-            </button>
-            <Link href='/login/forgot' className={styles.forgotPassword}>
-                Olvidé mi contraseña
-            </Link>
-            </form>
         </div>
-    </div>
-  )
-}
+    );
+};
--- app/register/register.tsx
+++ app/register/register.tsx
@@ -3,16 +3,13 @@ import React, { useState } from 'react';
 import styles from './register.module.css';
 import { UserCircleIcon, LockClosedIcon, ArrowRightOnRectangleIcon, EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { User } from '../lib/types';
 
 export const RegisterForm = () => {
-    const [userData, setUserData] = useState<User>({
+    const [userData, setUserData] = useState({
         email: '',
-        roles: ['standard'],
+        roles: ['admin'],
         password: '',
-        plainPassword: '',
-        profile: '',
-        media: ''
+        plainPassword: '',    
     });
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,87 +19,94 @@ export const RegisterForm = () => {
     
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-
+        
         if (userData.password !== userData.plainPassword) {
-          alert("Las contraseñas no coinciden.");
-          return;
+            alert("Las contraseñas no coinciden.");
+            return;
         }
-
-        try{
-            const response = await fetch('/api/users', {
+        
+        try {
+            const response = await fetch('http://localhost:8000/api/users', {
                 method: 'POST',
                 headers: {
-                  'Content-Type': 'application/json',
+                    'Content-Type': 'application/ld+json',
+                    'Accept': 'application/ld+json'
                 },
                 body: JSON.stringify(userData)
             });
-                console.log(response);
-              if (!response.ok) {
+            
+            if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
-              }
-        } catch(error){
-            alert("Hubo un error al registrarse");
-            return;
-        }
+            }
 
-    }
+            const data = await response.json(); // Obtener la respuesta
+            console.log('Respuesta del servidor:', data);
+            alert('Usuario creado exitosamente');
+            // Redirigir o limpiar el formulario después de la creación exitosa del usuario
+        } catch (error) {
+            console.error('Error:', error);
+            alert("Hubo un error al registrarse");
+        }
+    };
+
     const [showPassword, setShowPassword] = useState(false);
 
     const togglePasswordVisibility = () => {
         setShowPassword(!showPassword); // Alternar visibilidad de la contraseña
     };
-  return (
-    <div className={`${styles.loginContainer}`}>
-        <div className={styles.loginBox}>
-            <h1 className={styles.loginTitle}>Registrate</h1>
-            <form className={styles.loginForm}>
-            <div className={styles.inputGroup}>
-                <label className={styles.label} htmlFor="email">Correo electrónico</label>
-                <input type="email" id='email' name='email' placeholder="" className={styles.inputField} required onChange={handleChange}/>
-                <div className={styles.icon}>
-                    <UserCircleIcon width='24px'/>
-                </div>
+
+    return (
+        <div className={`${styles.loginContainer}`}>
+            <div className={styles.loginBox}>
+                <h1 className={styles.loginTitle}>Regístrate</h1>
+                <form className={styles.loginForm} onSubmit={handleSubmit}> {/* Asegúrate de usar onSubmit en el formulario */}
+                    <div className={styles.inputGroup}>
+                        <label className={styles.label} htmlFor="email">Correo electrónico</label>
+                        <input type="email" id='email' name='email' placeholder="" className={styles.inputField} required onChange={handleChange}/>
+                        <div className={styles.icon}>
+                            <UserCircleIcon width='24px'/>
+                        </div>
+                    </div>
+                    <div className={styles.inputGroup}>
+                        <label className={styles.label} htmlFor="password">
+                            Contraseña
+                            <span onClick={togglePasswordVisibility} style={{ cursor: 'pointer', marginLeft: '10px' }}>
+                                {showPassword ? <EyeSlashIcon width='24px' /> : <EyeIcon width='24px' />}
+                            </span>
+                        </label>
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            name='password'
+                            id='password'
+                            className={styles.inputField}
+                            required
+                            onChange={handleChange}
+                        />
+                        <div className={styles.icon}>
+                            <LockClosedIcon width='24px'/>
+                        </div>
+                    </div>
+                    <div className={styles.inputGroup}>
+                        <label className={styles.label} htmlFor="plainPassword">Repite tu contraseña</label>
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            name='plainPassword'
+                            id='plainPassword'
+                            className={styles.inputField}
+                            required
+                            onChange={handleChange}
+                        />
+                        <div className={styles.icon}>
+                            <LockClosedIcon width='24px'/>
+                        </div>
+                    </div>
+                    <button type="submit" className={styles.loginButton}>
+                        Registrarse
+                        <ArrowRightOnRectangleIcon width='24px' />
+                    </button>
+                    <Link href='/login' className={styles.forgotPassword}>Ya tengo una cuenta</Link>
+                </form>
             </div>
-            <div className={styles.inputGroup}>
-                <label className={styles.label} htmlFor="password">
-                    Contraseña
-                <span onClick={togglePasswordVisibility} style={{ cursor: 'pointer', marginLeft: '10px' }}>
-                    {showPassword ? <EyeSlashIcon width='24px' /> : <EyeIcon width='24px' />}
-                </span>
-                </label>
-                <input
-                    type={showPassword ? 'text' : 'password'}
-                    name='password'
-                    id='password'
-                    className={styles.inputField}
-                    required
-                    onChange={handleChange}
-                />
-                <div className={styles.icon}>
-                    <LockClosedIcon width='24px'/>
-                </div>
-            </div>
-            <div className={styles.inputGroup}>
-                <label className={styles.label} htmlFor="password2">Repite tu contraseña</label>
-                <input
-                    type={showPassword ? 'text' : 'password'}
-                    name='plainPassword'
-                    id='plainPassword'
-                    className={styles.inputField}
-                    required
-                    onChange={handleChange}
-                />
-                <div className={styles.icon}>
-                    <LockClosedIcon width='24px'/>
-                </div>
-            </div>
-            <button type="submit" className={styles.loginButton} onChange={handleSubmit}>
-                Registrarse
-                <ArrowRightOnRectangleIcon width='24px' />
-            </button>
-            <Link href='/login' className={styles.forgotPassword}>Ya tengo una cuenta</Link>
-            </form>
         </div>
-    </div>
-  )
+    )
 }
